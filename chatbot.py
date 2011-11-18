7abf4646840b0e65fc73278cf13c9d802f6159b4
SeanWeber <bluesmew@gmail.com>
2011-11-18 06:02:17 -0500

    added dictionary support

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,35 @@
+import urllib
+import zipfile
+import string
+
+dictionary = zipfile.ZipFile("dictionary.zip", "r").open("dictionary.txt", "r")
+#   Dictionary format:
+#   "English", word, type, "#", Definition
+
 def main():
     print "Hello"
-    process(raw_input())
+    talk(raw_input())
 
-def process(a):
-    print a
+def talk(a):
+    a = a.capitalize()
+    wordList = a.split(" ")
+    if (wordList[0] == "What") and (wordList[1] == "are" or "is"):
+        wordDefinition(wordList[-1])
+    if a == "goodbye":
+        endConversation()
+    talk(raw_input())
+
+def endConversation():
+    print "Bye"
+    exit()
+
+def wordDefinition(b):
+    for line in dictionary:
+        defList = line.split()
+        if b in defList[1] and defList[2] == "Noun":
+            s = str(defList[4:])
+            print "Its " + "".join(c for c in s if c.isalnum() or c.isspace())
+            break
 
 if __name__ == '__main__':
     main()
